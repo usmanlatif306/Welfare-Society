bf82d2947aa9d1b6ff40a6521d606a692229c020
Muhammad Usman Latif <usmanlatif603@gmail.com>
2021-09-29 17:22:13 +0500

    update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,6 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
 
-// const app = firebase.initializeApp({
-//   apiKey: "AIzaSyA6RDHNcBtQZUfmZzZuDllnQyz_HG6X8BY",
-//   authDomain: "welfare-society-ac74c.firebaseapp.com",
-//   projectId: "welfare-society-ac74c",
-//   storageBucket: "welfare-society-ac74c.appspot.com",
-//   messagingSenderId: "49813554003",
-//   appId: "1:49813554003:web:f4d0646d33aec5e35a57a6",
-// });
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
